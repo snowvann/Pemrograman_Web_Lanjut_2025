4511117c429b64d034fae72d72c4cb9f1a86e409
Danica Nasywa Putriniar <144027209+snowvann@users.noreply.github.com>
2025-03-03 19:12:17 +0700

    Delete Minggu 3 directory

--- Minggu 3/resources/views/products/baby-kid.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Beauty & Health</title>
-</head>
-<body>
-    <h1>Beauty & Health Products</h1>
-</body>
-</html>
--- Minggu 3/resources/views/products/food-beverage.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Food & Beverage</title>
-</head>
-<body>
-    <h1>Food & Beverage Products</h1>
-</body>
-</html>
