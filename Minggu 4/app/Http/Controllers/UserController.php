68c99cf52fdca7b05a5864d6726ca9ca176e659e
snowvann <danicanasywa5a@gmail.com>
2025-03-06 14:26:16 +0700

    jobsheet 4

---
+++ Minggu 4/app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\SUpport\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index(){
+        $data = [
+            'level_id' => 2, 
+            'username' => 'manager_dua', 
+            'nama' => 'Manager 2', 
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+        $user = UserModel::all(); 
+        return view('user', ['data' => $user]);
+    }
+}
