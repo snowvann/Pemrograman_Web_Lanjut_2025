787472b15d96e9b136716c2446bb39c203e00e25
snowvann <danicanasywa5a@gmail.com>
2025-03-08 00:59:09 +0700

    Jobsheet 4_Praktikum 2.4

--- Minggu 4/app/Http/Controllers/UserController.php
+++ Minggu 4/app/Http/Controllers/UserController.php
@@ -10,7 +10,19 @@ class UserController extends Controller
 {
     public function index()
     {
-        $jumlahPengguna = UserModel::where('level_id', 2)->count(); 
-        return view('user', ['jumlahPengguna' => $jumlahPengguna]);
-    }
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33', 
+                'nama' => 'Manager Tiga Tiga',
+                'level_id' => 2, // Kriteria pencarian
+            ],
+            [
+                'password' => Hash::make('12345') // Data tambahan jika tidak ditemukan
+            ]
+        );
+    
+        $user->save(); // Simpan data baru ke database
+    
+        return view('user', ['data' => $user]); // Mengirim data ke tampilan
+    }    
 }
