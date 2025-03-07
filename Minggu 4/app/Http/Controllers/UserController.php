daa67bb9e3b9866e59b80cd3599a85fe8777144a
snowvann <danicanasywa5a@gmail.com>
2025-03-07 23:59:00 +0700

    Jobsheet 4_Praktikum 2.2

--- Minggu 4/app/Http/Controllers/UserController.php
+++ Minggu 4/app/Http/Controllers/UserController.php
@@ -10,9 +10,7 @@ class UserController extends Controller
 {
     public function index() // membuat method untuk mengakses UserModel
     {
-        $user = UserModel::findOr(20, ['username','nama'],function(){ // mencari data user berdasarkan primary key (user_id = 1), hanya mengambil kolom username dan nama.
-            abort(404); // jika tidak ditemukan, jalankan callback yang memanggil. abort 404 = mengehntikan eksekusi dan mengembalikan HTTP response 4040 (not found)
-        }); // mengambil semua data user
+        $user = UserModel::where('username', 'manager9')->firstOrFail(); // mengambil semua data user
         return view('user', ['data' => $user]); // mengirim data ke tampilan
     }
 }
