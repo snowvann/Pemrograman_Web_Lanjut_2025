e5576e4e54b7d6df1879ea2bed9970cba2ea6a0e
snowvann <danicanasywa5a@gmail.com>
2025-03-13 19:06:49 +0700

    Jobsheet 4

--- Minggu 4/app/Http/Controllers/UserController.php
+++ Minggu 4/app/Http/Controllers/UserController.php
@@ -10,42 +10,56 @@ use function Laravel\Prompts\password;
 
 class UserController extends Controller
 {
-    public function index() // membuat method untuk mengakses UserModel
+    public function index() 
     {
-        // // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3', 
-        //     'password' => Hash::make('12345')
-        // ]; 
-        // UserModel::create($data); // menambahkan data ke tabel m_user
-        
-        // coba akses model UserModel
-        // $user = UserModel::where('level_id',2)->count(); 
-        // dd($user);
-        // return view('user', ['data' => $user]); // mengirim data ke tampilan
+        $user = UserModel::with('level')->get();
+        return view('user', ['data' => $user]);
+    }
 
-        // $jumlahPengguna = UserModel::where('level_id', 2)->count();
-        // return view('user', ['jumlahPengguna' => $jumlahPengguna]);
+    public function tambah() 
+    {
+        return view('user_tambah');
+    }
+    
+    public function tambah_simpan(Request $request) 
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
 
-        $user = UserModel::create( 
-            [
-                'username' => 'manager11', 
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        ); 
+        return redirect('/user');
+    }
 
-        $user->username = 'manager12';
+    public function ubah($id) 
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request) 
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
 
         $user->save();
 
-        $user->wasChanged(); //true
-        $user->wasChanged('username'); //true
-        $user->wasChanged(['username', 'level_id']); //true
-        $user->wasChanged('nama'); //false
-        dd($user->wasChanged(['nama', 'username'])); //true
+        return redirect('/user');
     }
+
+    public function hapus($id) 
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
+    }
+
+
 }
--- Minggu 4/resources/views/user.blade.php
+++ Minggu 4/resources/views/user.blade.php
@@ -1,26 +1,34 @@
-<! DOCTYPE html>
+<!DOCTYPE html>
     <html>
+        <head>
+            <title>Data Level Pengguna</title>
+        </head>
 
-    <head>
-        <title>Data Level Pengguna</title>
-    </head>
-
-    <body>
+        <body>
         <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
+        <a href="http://localhost/Pemrograman_Web_Lanjut_2025/Minggu%204/public/user/tambah">+ Tambah User</a>
+        <table border="1" cellpadding="2" cellspacing='0'> 
             <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>IDPengguna</td>
+                <td>Kode Level</td>
+                <td>Nama Level</td>
             </tr>
+            @foreach ($data as $d)
             <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama}}</td>
-                <td>{{ $data->level_id}}</td>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level->level_kode }}</td>
+                <td>{{ $d->level->level_nama }}</td>
+            <td>
+            <a href="http://localhost/Pemrograman_Web_Lanjut_2025/Minggu%204/public/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/Pemrograman_Web_Lanjut_2025/Minggu%204/public/user/hapus/{{ $d->user_id }}">Hapus</a>
+            </td>
             </tr>
+        @endforeach
         </table>
     </body>
-
     </html>
