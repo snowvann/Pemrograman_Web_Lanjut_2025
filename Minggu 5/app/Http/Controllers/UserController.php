c9f1194d9fc9b3bb85550873f426b0c58fbe8788
snowvann <danicanasywa5a@gmail.com>
2025-03-24 20:50:18 +0700

    Josbheet 5 Prak 4

--- Minggu 5/app/Http/Controllers/UserController.php
+++ Minggu 5/app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
     {
         $breadcrumb = (object) [
             'title' => 'Daftar User',
-            'list'  => ['Home', 'User']
+            'list' => ['Home', 'User']
         ];
 
         $page = (object) [
@@ -22,7 +22,14 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all(); // ambil data level untuk filter level
+
+        return view('user.index', [
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'level' => $level,
+            'activeMenu' => $activeMenu
+        ]);
     }
 
     // Ambil data user dalam bentuk json untuk DataTables
@@ -30,6 +37,11 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        // Filter data user bedasarkan level_id
+        if ($request->has('level_id')) {
+            $users->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             // Menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
@@ -170,4 +182,5 @@ public function edit(string $id)
             return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
         }
     }
+    
 }
