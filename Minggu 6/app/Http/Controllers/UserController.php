0c76d210c3e56a47433578dc9cba42eda0b0b6fa
snowvann <danicanasywa5a@gmail.com>
2025-04-03 12:22:15 +0700

    JS6 Prak 2

--- Minggu 6/app/Http/Controllers/UserController.php
+++ Minggu 6/app/Http/Controllers/UserController.php
@@ -41,18 +41,21 @@ class UserController extends Controller
             ->with('level');
 
         // Filter data user bedasarkan level_id
-        if ($request->has('level_id')) {
+        if ($request->level_id) {
             $users->where('level_id', $request->level_id);
         }
 
         return DataTables::of($users)
             ->addIndexColumn()
             ->addColumn('aksi', function ($user) {
-                $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
-                $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
-                $btn .= '<form class="d-inline-block" method="POST" action="'.url('/user/'.$user->user_id).'">'. 
-                    csrf_field() . method_field('DELETE') . 
-                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
+                $btn = '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/show_ajax').'\')" class="btn btn-info btn-sm">Detail</button> '; 
+                $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/edit_ajax').'\')" class="btn btn-warning btn-sm">Edit</button> '; 
+                $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/delete_ajax').'\')" class="btn btn-danger btn-sm">Hapus</button> ';
+                // $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
+                // $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
+                // $btn .= '<form class="d-inline-block" method="POST" action="'.url('/user/'.$user->user_id).'">'. 
+                //     csrf_field() . method_field('DELETE') . 
+                //     '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
                 return $btn;
             })
             ->rawColumns(['aksi'])
@@ -236,5 +239,43 @@ class UserController extends Controller
         }
     }
 
+    public function update_ajax(Request $request, $id){ 
+        // cek apakah request dari ajax 
+        if ($request->ajax() || $request->wantsJson()) { 
+            $rules = [ 
+                'level_id' => 'required|integer', 
+                'username' => 'required|max:20|unique:m_user,username,'.$id.',user_id', 
+                'nama' => 'required|max:100', 
+                'password' => 'nullable|min:6|max:20' 
+            ];
+            // use Illuminate\Support\Facades\Validator; 
+            $validator = Validator::make($request->all(), $rules); 
+            
+            if ($validator->fails()) { 
+                return response()->json([ 
+                    'status' => false, // respon json, true: berhasil, false: gagal 
+                    'message' => 'Validasi gagal.', 
+                    'msgField' => $validator->errors() // menunjukkan field mana yang error 
+                    ]); } 
+                    
+                    $check = UserModel::find($id); 
+                    if ($check) { if(!$request->filled('password') ){ // jika password tidak diisi, maka hapus dari request 
+                        $request->request->remove('password'); 
+                    } 
+                    
+                    $check->update($request->all()); 
+                    return response()->json([ 
+                        'status' => true, 
+                        'message' => 'Data berhasil diupdate' 
+                    ]); 
+                } 
+                else{ return response()->json([ 
+                    'status' => false, 
+                    'message' => 'Data tidak ditemukan' 
+                ]); 
+            } 
+        } 
+        return redirect('/');
+    }
 
 }
