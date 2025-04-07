5daeea5950e8e9e6df369698d0e1c68f6065aee2
snowvann <danicanasywa5a@gmail.com>
2025-04-07 11:15:15 +0700

    revisi js 6 tadi error

--- Minggu 6/resources/views/user/index.blade.php
+++ Minggu 6/resources/views/user/index.blade.php
@@ -6,7 +6,7 @@
         <h3 class="card-title">{{ $page->title }}</h3>
         <div class="card-tools">
             <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
-            <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+            <button onclick="modalAction('{{ url('/user/create_ajax/') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
         </div>
     </div>
     <div class="card-body">
