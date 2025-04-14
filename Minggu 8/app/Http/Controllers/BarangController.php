a455d02f9f7aa92f8060460d903e8036e1a08e49
snowvann <danicanasywa5a@gmail.com>
2025-04-15 06:24:56 +0700

    JS 8 Prak 2 Tugas 2

--- Minggu 8/app/Http/Controllers/BarangController.php
+++ Minggu 8/app/Http/Controllers/BarangController.php
@@ -8,7 +8,10 @@ use App\Models\LevelModel;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
 
 class BarangController extends Controller
 {
@@ -252,4 +255,72 @@ class BarangController extends Controller
 
         return redirect('/');
     }
+
+    public function export_excel()
+{
+    try {
+        // Ambil data barang yang akan diekspor
+        $barang = BarangModel::select('kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual')
+            ->orderBy('kategori_id')
+            ->with('kategori')
+            ->get();
+
+        // Load library PhpSpreadsheet
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // Set header kolom
+        $sheet->setCellValue('A1', 'No');
+        $sheet->setCellValue('B1', 'Kode Barang');
+        $sheet->setCellValue('C1', 'Nama Barang');
+        $sheet->setCellValue('D1', 'Harga Beli');
+        $sheet->setCellValue('E1', 'Harga Jual');
+        $sheet->setCellValue('F1', 'Kategori');
+
+        // Format header bold
+        $sheet->getStyle('A1:F1')->getFont()->setBold(true);
+
+        // Isi data barang
+        $no = 1;
+        $baris = 2;
+        foreach ($barang as $value) {
+            $sheet->setCellValue('A' . $baris, $no);
+            $sheet->setCellValue('B' . $baris, $value->barang_kode);
+            $sheet->setCellValue('C' . $baris, $value->barang_nama);
+            $sheet->setCellValue('D' . $baris, $value->harga_beli);
+            $sheet->setCellValue('E' . $baris, $value->harga_jual);
+            $sheet->setCellValue('F' . $baris, $value->kategori->kategori_nama);
+            $baris++;
+            $no++;
+        }
+
+        // Set auto size untuk kolom
+        foreach (range('A', 'F') as $columnID) {
+            $sheet->getColumnDimension($columnID)->setAutoSize(true);
+        }
+
+        // Set title sheet
+        $sheet->setTitle('Data Barang');
+
+        // Generate filename
+        $filename = 'Data_Barang_' . date('Y-m-d_H-i-s') . '.xlsx';
+
+        // Set header untuk download file
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+        header('Cache-Control: cache, must-revalidate');
+        header('Pragma: public');
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save('php://output');
+        exit;
+    } catch (\Exception $e) {
+        Log::error("Gagal mengekspor Excel: " . $e->getMessage());
+        return response()->json(['error' => 'Ekspor gagal. Silakan coba lagi.'], 500);
+    }
+}
+
 }
--- Minggu 8/resources/views/barang/index.blade.php
+++ Minggu 8/resources/views/barang/index.blade.php
@@ -7,6 +7,7 @@
         <div class="card-tools">
         <a href="{{ url('/barang/import/') }}" class="btn btn-primary">Import</a>
         <a href="{{ url('/barang/create_ajax/') }}" class="btn btn-primary">Tambah Data</a> 
+        <a href="{{ url('/barang/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i> Export Barang</a>
             
         <!-- <button onclick="modalAction('{{ url('/barang/import/') }}')" class="btn btn-info">Import Barang</button> 
         <a href="{{ url('/barang/create') }}" class="btn btn-primary">Tambah Data</a> 
